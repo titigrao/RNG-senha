4080161b9cb5d37fb7d2ec00bb765b31cef9e3d7
Titigas <127964804+titigrao@users.noreply.github.com>
2024-07-30 07:51:58 -0300

    l

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('forte');
     } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tentropia <= 57){
+    } else if (entropia <= 57){
         forcaSenha.classList.add('fraca');
      }
      const valorEntropia = document.querySelector('.entropia');
